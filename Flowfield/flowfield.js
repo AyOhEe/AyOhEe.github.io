d05a8d30f47e767e400e1fa51e04876bdde7f735
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-10-12 00:48:27 +0100

    Updated flowfields

--- Flowfield/flowfield.js
+++ Flowfield/flowfield.js
@@ -1,19 +1,45 @@
-let REDRAW_INTERVAL = 20; //draw at 50fps
+const REDRAW_INTERVAL = 20; //draw at 50fps
 
-function sampleFlow(screen_height, x, y){
-    let g_y = ((screen_height/2) - y) / (screen_height/2);
-    let g_x = 1 - Math.abs(g_y);
+function sampleFlow(x, y){
+    let g_y = 0;
+    let g_x = 0;
 
     return {
         x : g_x,
         y : g_y
-    };
-};
+    }
+}
+
+class Particle {
+    constructor(x, y, x_vel, y_vel, size=1){
+        this.x = x;
+        this.y = y;
+        this.x_vel = x_vel;
+        this.y_vel = y_vel;
+    
+        this.size = size;
+    }
+
+    draw = (ctx, canvas) => {
+        //draw the line of length px
+        ctx.beginPath();
+        ctx.arc((this.x % canvas.offsetWidth), (this.y % canvas.offsetHeight), this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+    update = () => {
+        this.x += this.x_vel * (1 / REDRAW_INTERVAL);
+        this.y += this.y_vel * (1 / REDRAW_INTERVAL);
+
+        let flow = sampleFlow(this.x, this.y);
+        this.x_vel += flow.x * (1 / REDRAW_INTERVAL);
+        this.y_vel += flow.y * (1 / REDRAW_INTERVAL);
+    }
+}
 
 
 function makeFlowfield(canvas) {
     //handle drawing the contents every so often
-    window.setInterval(drawFlowfield, REDRAW_INTERVAL);
+    window.setInterval(drawParticles, REDRAW_INTERVAL);
     function createLineGradient(ctx, start, end, length){
         const gradient = ctx.createLinearGradient(0, 0, length, 0);
 
@@ -22,8 +48,7 @@ function makeFlowfield(canvas) {
         gradient.addColorStop(1, end);
 
         return gradient;
-    };
-
+    }
     function drawAtAngle(ctx, x, y, theta, length){
         //create save to restore to after transformations
         ctx.save();
@@ -40,7 +65,7 @@ function makeFlowfield(canvas) {
 
         //undo the transformations
         ctx.restore();
-    };
+    }
     function showFlowPaths(ctx){
         var length = 20;
 
@@ -58,29 +83,55 @@ function makeFlowfield(canvas) {
                 drawAtAngle(ctx, x, y, Math.atan2(grad.y, grad.x), length);
             };
         };
-    };
+    }
+  
+    let particles = []
 
-    function drawFlowfield(){
+    for(let i = 0; i < 500; i++){
+        particles.push(
+            new Particle(
+                (Math.random() * canvas.offsetWidth),
+                (Math.random() * canvas.offsetHeight),
+                (Math.random() * 20) + 30,
+                (Math.random() * 20) - 10,
+                0.8    
+            )
+        );
+    }
+
+    function updateParticles(){
+        for(let i = 0; i < particles.length; i++){
+            particles[i].update();
+        }
+    }
+
+    function drawParticles(){
         //get the context and clear the drawing
         var ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = "#ff6666"
         
-        showFlowPaths(ctx);
-    };
+        //showFlowPaths(ctx);
+
+        updateParticles();
+        for(let i = 0; i < particles.length; i++){
+            particles[i].draw(ctx, canvas);
+        }
+    }
 
     //resize the canvas on window resize dynamically
     window.addEventListener('resize', resizeCanvas, false);
     function resizeCanvas() {
         canvas.width = canvas.parentElement.offsetWidth;
         canvas.height = canvas.parentElement.offsetHeight;
-    };
+    }
 
     //resize the canvas initially to ensure proper scaling, then redraw the contents properly
     resizeCanvas();
-    drawFlowfield();
-};
+    drawParticles();
+}
 
 var canvases = document.getElementsByClassName('flowfield');
 for(var i = 0; i < canvases.length; i++) {
     makeFlowfield(canvases[i]);
-};
+}
